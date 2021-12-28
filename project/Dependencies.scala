e742a1032412d31560429ebee845af0c8664aae5
Scala Steward <me@scala-steward.org>
2021-12-28 04:06:08 +0100

    Update akka-actor, akka-slf4j, akka-stream to 2.6.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val tapir = "0.17.20"
     val `tapir-swagger` = "0.18.3"
     val `akka-http` = "10.1.15"
-    val akka = "2.5.32"
+    val akka = "2.6.18"
     val circe = "0.14.1"
     val scalikejdbc = "3.5.0"
     val logback = "1.2.10"
