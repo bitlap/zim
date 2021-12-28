e0e00317ca427eeeea7abe1d54f94ef9883d0940
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-28 03:55:13 +0100

    Update mysql-connector-java to 8.0.27 (#10)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val scalikejdbc = "3.5.0"
     val logback = "1.2.10"
     val config = "1.4.1"
-    val mysql = "8.0.25"
+    val mysql = "8.0.27"
     val `zio-interop-reactiveStreams` = "1.3.8"
   }
 
