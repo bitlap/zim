110dbf28e538847d685315d2fd2d10a205108822
梦境迷离 <dreamylost@outlook.com>
2022-01-25 10:59:00 +0800

    split auth module (#75)

---
+++ modules/zim-auth/src/main/scala/org/bitlap/zim/auth/CookieAuthority.scala
@@ -0,0 +1,52 @@
+package org.bitlap.zim.auth
+
+import org.bitlap.zim.domain.input.UserSecurity
+import org.bitlap.zim.domain.input.UserSecurity.UserSecurityInfo
+import org.bitlap.zim.domain.ZimError.Unauthorized
+import zio.IO
+
+import java.util.Base64
+import scala.concurrent.Future
+import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Cookie based authentication
+ *
+ * @author 梦境迷离
+ * @version 1.0,2022/1/25
+ */
+trait CookieAuthority {
+
+  lazy val zioRuntime: zio.Runtime[zio.ZEnv] = zio.Runtime.default
+
+  type AuthorityFunction = (String, String) => IO[Throwable, (Boolean, Option[UserSecurityInfo])]
+
+  /**
+   * 鉴权，目前写死，后续使用Redis缓存用户信息，没有再查库
+   * @param token
+   * @return
+   */
+  def authenticate(token: UserSecurity)(auth: AuthorityFunction): Future[Either[Unauthorized, UserSecurityInfo]] =
+    Future {
+      val tk = if (token.cookie.trim.contains(" ")) token.cookie.trim.split(" ")(1) else token.cookie
+      val secret: String = Try(new String(Base64.getDecoder.decode(tk))).getOrElse(null)
+      if (secret == null || secret.isEmpty) {
+        Left(Unauthorized())
+      } else if (secret.contains(":")) {
+        val usernamePassword = secret.split(":")
+        val email = usernamePassword(0)
+        val passwd = usernamePassword(1)
+        val ret = auth(email, passwd)
+        val (check, user) = zioRuntime.unsafeRun(ret)
+        if (!check) {
+          Left(Unauthorized())
+        } else {
+          Right(user.orNull)
+        }
+      } else {
+        Left(Unauthorized())
+      }
+    }
+
+}
