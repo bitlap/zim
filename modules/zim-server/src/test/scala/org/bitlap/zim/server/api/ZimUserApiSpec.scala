ca3741f535f1962a2e341fff7e365a10e21740d6
梦境迷离 <dreamylost@outlook.com>
2022-02-11 15:26:16 +0800

    add test for routes

--- modules/zim-server/src/test/scala/org/bitlap/zim/server/api/ZimUserApiSpec.scala
+++ modules/zim-server/src/test/scala/org/bitlap/zim/server/api/ZimUserApiSpec.scala
@@ -1,14 +1,20 @@
 package org.bitlap.zim.server.api
 
+import akka.http.scaladsl.marshalling.{ Marshaller, ToEntityMarshaller }
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.server._
-import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.testkit.{ RouteTestTimeout, ScalatestRouteTest }
+import akka.testkit.TestDuration
+import io.circe.syntax.EncoderOps
+import org.bitlap.zim.domain.input.RegisterUserInput
 import org.bitlap.zim.server.api.ZimUserApi.ZZimUserApi
 import org.bitlap.zim.server.application.TestApplication
 import org.bitlap.zim.server.application.impl.ApiService
 import org.bitlap.zim.server.configuration.ZimServiceConfiguration
 import org.bitlap.zim.server.repository.TangibleUserRepository
 import zio.{ TaskLayer, ZIO }
-import akka.util.Timeout
+
 import scala.concurrent.duration._
 
 /**
@@ -20,7 +26,7 @@ import scala.concurrent.duration._
  */
 class ZimUserApiSpec extends TestApplication with ZimServiceConfiguration with ScalatestRouteTest {
 
-  implicit val timeout = Timeout(10000.milliseconds)
+  implicit val timeout = RouteTestTimeout(5.seconds.dilated)
 
   val api: TaskLayer[ZZimUserApi] = ZimUserApi.make(ApiService.make(userApplicationLayer), materializerLayer)
 
@@ -29,16 +35,29 @@ class ZimUserApiSpec extends TestApplication with ZimServiceConfiguration with S
     unsafeRun(s)
   }
 
-  "The ZimUserApi service" should "return a json for GET empty data" in {
+  "getOne" should "OK for GET empty data" in {
     Get("/user/getOne?id=1") ~> getRoute(_.userGetRoute) ~> check {
       responseAs[String] shouldEqual """{"data":null,"msg":"操作成功","code":0}"""
     }
   }
 
-  "The ZimUserApi service" should "return a json for GET" in {
+  "getOne" should "OK for GET" in {
     unsafeRun(TangibleUserRepository.saveUser(mockUser).provideLayer(userLayer).runHead)
     Get("/user/getOne?id=1") ~> getRoute(_.userGetRoute) ~> check {
       responseAs[String] shouldEqual """{"data":{"id":1,"username":"zhangsan","password":"123456","sign":"","avatar":"/static/image/avatar/avatar(3).jpg","email":"dreamylost@outlook.com","createDate":"2022-02-11 00:00:00","sex":1,"status":"nonactivated","active":"1ade893a1b1940a5bb8dc8447538a6a6a18ad80bcf84437a8cfb67213337202d"},"msg":"操作成功","code":0}"""
     }
   }
+
+  "register" should "OK for POST" in {
+    implicit val m: ToEntityMarshaller[RegisterUserInput] = Marshaller.withFixedContentType(`application/json`) { f =>
+      HttpEntity(`application/json`, f.asJson.noSpaces)
+    }
+
+    Post("/user/register", RegisterUserInput("username", "password", "dreamylost@outlook.com")) ~> getRoute(
+      _.registerRoute
+    ) ~> check {
+      responseAs[String] shouldEqual """{"data":true,"msg":"操作成功","code":0}"""
+    }
+  }
+
 }
