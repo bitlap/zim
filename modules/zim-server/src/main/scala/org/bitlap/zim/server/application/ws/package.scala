951137bef4d7c395e784176809e0b1754f9ed47d
梦境迷离 <dreamylost@outlook.com>
2022-03-19 23:16:46 +0800

    update Copyright

--- modules/zim-server/src/main/scala/org/bitlap/zim/server/application/ws/package.scala
+++ modules/zim-server/src/main/scala/org/bitlap/zim/server/application/ws/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 bitlap
+ * Copyright 2022 bitlap
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- project/Information.scala
+++ project/Information.scala
@@ -1,8 +1,8 @@
-import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.headerCreateAll
 import sbt.Keys._
-import sbt.librarymanagement.License
-import sbt.{ url, Compile, Def, Developer, ScmInfo }
-
+import sbt.{ url, Def, Developer, ScmInfo }
+import sbt.URL
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.headerCreateAll
+import sbt.Compile
 /**
  * @author 梦境迷离
  * @since 2022/1/15
@@ -14,10 +14,10 @@ object Information {
     Compile / compile := (Compile / compile).dependsOn(Compile / headerCreateAll).value,
     organization := "org.bitlap",
     organizationName := "bitlap",
-    startYear := Some(2021),
+    startYear := Some(2022),
     description := "zim is a functional-style, asynchronous and streaming IM based on scala and zio",
     homepage := Some(url(s"https://github.com/bitlap/zim")),
-    licenses += License.Apache2,
+    licenses += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt")),
     developers := List(
       Developer(
         "jxnu-liguobin",
