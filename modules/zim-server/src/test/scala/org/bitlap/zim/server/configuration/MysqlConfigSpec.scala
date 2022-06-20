55d659cabe2957d8cd095b912fb7eae9bfe3ef1c
GW00250534 <GW00250534@tech.gwm.cn>
2022-06-20 17:05:44 +0800

    update database connect test

--- modules/zim-server/src/test/scala/org/bitlap/zim/server/configuration/MysqlConfigSpec.scala
+++ modules/zim-server/src/test/scala/org/bitlap/zim/server/configuration/MysqlConfigSpec.scala
@@ -1,6 +1,5 @@
 package org.bitlap.zim.server.configuration
 
-import org.bitlap.zim.infrastructure.properties.MysqlConfigurationProperties
 import org.bitlap.zim.server.BaseSuit
 import scalikejdbc._
 import zio.test._
@@ -10,28 +9,12 @@ object MysqlConfigSpec extends BaseSuit {
 
 
   def spec = suite("MysqlConfigSpec")(
-    test("test the mysql connect working state") {
+    test("test the database connect working state") {
       assert(isConnected)(equalTo(true))
     }
   )
 
-  val mysqlConfigurationProperties = MysqlConfigurationProperties()
-
-  ConnectionPool.add(
-    Symbol(mysqlConfigurationProperties.databaseName),
-    mysqlConfigurationProperties.url,
-    mysqlConfigurationProperties.user,
-    mysqlConfigurationProperties.password,
-    ConnectionPoolSettings(
-      initialSize = mysqlConfigurationProperties.initialSize,
-      maxSize = mysqlConfigurationProperties.maxSize,
-      connectionTimeoutMillis = mysqlConfigurationProperties.connectionTimeoutMillis,
-      validationQuery = mysqlConfigurationProperties.validationQuery,
-      driverName = mysqlConfigurationProperties.driverName
-    )
-  )
-
-  val isConnected: Boolean = NamedDB(Symbol(mysqlConfigurationProperties.databaseName)).conn.isValid(0)
+  val isConnected: Boolean = NamedDB(Symbol(h2ConfigurationProperties.databaseName)).conn.isValid(0)
 
 
 }
