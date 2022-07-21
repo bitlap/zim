6d21e0901a2fdb6db1bc815b4a6a2984700fb6ac
Scala Steward <scala_steward@virtuslab.com>
2022-07-21 12:25:23 +0000

    Update smt-cacheable-caffeine to 0.7.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val `zio-actors`                  = "0.0.9"
     val refined                       = "0.10.0"
     val `zio-schema`                  = "0.1.9"
-    val `smt-cacheable`               = "0.7.4"
+    val `smt-cacheable`               = "0.7.5"
   }
 
   lazy val zioDeps: Seq[ModuleID] = Seq(
