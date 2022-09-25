f439a7b51f3fec3aa938b8b4ebefe9baec0b6ee7
梦境迷离 <dreamylost@outlook.com>
2022-09-25 12:44:17 +0800

    Deploy 0.5.1 (#209)
    * deploy
    
    * deploy0.5.1

--- native-packager-deploy.sh
+++ native-packager-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+sbt clean
+
 sbt universal:packageBin
 # FIXME: To copy static file from prefix version.
 version=$1
@@ -7,7 +10,7 @@ pre_version=$2
 
 scp ~/Projects/zim/modules/zim-server/target/universal/zim-server-$version.zip root@your_ip:/home/zim/zim-server-$version.zip
 
-ssh root@your_ip "ps -ef | grep -w java | grep -v grep | awk '{print $2}' | xargs kill -9; sleep 2; cd /home/zim; unzip -o zim-server-$version.zip; nohup ./zim-server-$version/bin/zim-server -Xms512M > zim.log 2>&1 &"
-
+ssh root@your_ip "ps -ef | grep -w java | grep -v grep | awk '{print $2}' | xargs kill -9;"
+ssh root@your_ip "cd /home/zim; unzip -o zim-server-$version.zip; nohup ./zim-server-$version/bin/zim-server -Xms512M > zim.log 2>&1 &"
 # FIXME: Static files are in the service's current directory, and each deployment will use a new directory.
 ssh root@your_ip "cp -r /home/zim/zim-server-$pre_version/bin/static/  /home/zim/zim-server-$version/bin/"
