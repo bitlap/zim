5bb702ada46d4983369f09fbf4ca0bb930bc4002
梦境迷离 <dreamylost@outlook.com>
2022-12-02 15:19:44 +0800

    publish docker image:0.6.0

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#sbt clean compile
+
+cp modules/zim-server/src/main/resources/application-docker.conf modules/zim-server/src/main/resources/application-docker-copy.conf
+cp modules/zim-server/src/main/resources/application.conf modules/zim-server/src/main/resources/application-copy.conf
+mv modules/zim-server/src/main/resources/application-docker.conf modules/zim-server/src/main/resources/application.conf
+
 # it can only executed for local pc, and used local image
 IFS=$'\n'
 maybeOld=`docker ps | grep liguobin/zim | awk '{print $1}'`
@@ -12,6 +18,11 @@ fi
 
 
 sbt docker:publishLocal
+
+mv modules/zim-server/src/main/resources/application-copy.conf modules/zim-server/src/main/resources/application.conf
+mv modules/zim-server/src/main/resources/application-docker-copy.conf modules/zim-server/src/main/resources/application-docker.conf
+mv modules/zim-server/target/scala-2.13/classes/application-copy.conf modules/zim-server/target/scala-2.13/classes/application.conf
+
 
 if [[ -d "/tmp/mysql/datadir/" ]];then
   rm -rf /tmp/mysql/datadir/*
