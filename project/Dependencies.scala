c926408f054d46c90519295c381deee7c46bc862
Scala Steward <scala_steward@virtuslab.com>
2023-04-25 13:26:52 +0000

    Update tapir-akka-http-server, ... to 1.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   object Version {
     val zio                           = "2.0.13"
     val `zio-logging`                 = "2.1.11"
-    val tapir                         = "1.2.12"
+    val tapir                         = "1.2.13"
     val `akka-http`                   = "10.2.10"
     val akka                          = "2.6.20"
     val circe                         = "0.14.3"
