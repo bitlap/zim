087bd99c5a49e2ec3b483d9833a274420ecb359c
梦境迷离 <dreamylost@outlook.com>
2023-05-08 23:57:10 +0800

    fix format code

--- project/Commands.scala
+++ project/Commands.scala
@@ -9,15 +9,12 @@ object Commands {
 
   val FmtSbtCommand = Command.command("fmt")(state => "scalafmtSbt" :: "scalafmtAll" :: state)
 
-  val FixSbtCommand = Command.command("fix")(state => "scalafixEnable" :: "scalafixAll RemoveUnused" :: state)
-
   val FmtSbtCheckCommand =
     Command.command("check")(state => "scalafmtSbtCheck" :: "scalafmtCheckAll" :: state)
 
   val value = Seq(
     FmtSbtCommand,
-    FmtSbtCheckCommand,
-    FixSbtCommand
+    FmtSbtCheckCommand
   )
 
 }
