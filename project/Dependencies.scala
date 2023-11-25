256eb7bd2fae42508b9c1eb8e87f37bfa2ecfb81
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-11-25 15:47:39 +0100

    Update zio-logging to 2.1.16 (#411)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
 
   object Version {
     val zio                           = "2.0.19"
-    val `zio-logging`                 = "2.1.15"
+    val `zio-logging`                 = "2.1.16"
     val tapir                         = "1.7.0"
     val `akka-http`                   = "10.5.0"
     val akka                          = "2.8.0"
