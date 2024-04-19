aa86154bf4677b6dbaff307277998add0b6c3b7d
Scala Steward <scala_steward@virtuslab.com>
2024-04-19 14:09:06 +0000

    Update logback-classic to 1.5.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val akka                          = "2.8.0"
     val circe                         = "0.14.3"
     val scalikejdbc                   = "4.2.1"
-    val logback                       = "1.5.5"
+    val logback                       = "1.5.6"
     val config                        = "1.4.3"
     val `zio-interop-reactiveStreams` = "2.0.2"
     val mysql                         = "8.0.32"
