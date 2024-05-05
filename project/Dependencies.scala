8aac7f24df8f7158c4450df26f93bdef0a99be8c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-05 07:24:19 +0200

    Update log4cats-slf4j to 2.7.0 (#453)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
     val `sttp-apispec`                = "0.6.3"
     val redis4cats                    = "1.4.1"
     val `zio-interop-cats`            = "23.0.03"
-    val `log4cats-slf4j`              = "2.5.0"
+    val `log4cats-slf4j`              = "2.7.0"
     val `zio-redis`                   = "0.2.0"
     val `zio-crypto`                  = "0.0.0+102-23de4b47-SNAPSHOT" // Experimental
   }
