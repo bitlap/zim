8ee617c284b9edf5c9667bb5203168a9c471f8a4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-09 15:38:54 +0100

    Update zio, zio-test, zio-test-sbt to 2.1.12 (#513)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ import sbt._
 object Dependencies {
 
   object Version {
-    val zio                           = "2.1.11"
+    val zio                           = "2.1.12"
     val `zio-logging`                 = "2.3.2"
     val tapir                         = "1.7.0"
     val `akka-http`                   = "10.5.0"
